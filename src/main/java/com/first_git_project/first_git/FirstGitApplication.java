e6490e84b3a561299f0ac82972a82c5525f02700
Kristian351 <kristianyanchevv@abv.bg>
2021-05-17 19:12:49 +0300

    Initial commit

---
+++ src/main/java/com/first_git_project/first_git/FirstGitApplication.java
@@ -0,0 +1,13 @@
+package com.first_git_project.first_git;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstGitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/first_git_project/first_git/FirstGitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.first_git_project.first_git;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstGitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
